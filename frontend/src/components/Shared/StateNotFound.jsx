2515c2f1d9ebe254e14d7970174b64a131b9a6ae
Mohammed Foud <mfoud444@gmail.com>
2024-11-26 23:05:55 +0300

    first commit

--- frontend/src/components/Shared/StateNotFound.jsx
+++ frontend/src/components/Shared/StateNotFound.jsx
@@ -6,11 +6,11 @@ const StateNotFound = ({ message, onBack }) => {
       <img  src={NotFound} alt="Not Found" className=" w-40 h-40 text-center rounded-lg" />
       <h2 className=" font-bold text-red-700 text-3xl"> Not Found!!</h2>
       <p>{message || "We couldn't find what you were looking for."}</p>
-      <div className="state-actions">
+      {/* <div className="state-actions">
         <button onClick={onBack}  className="btn btn-secondary px-28 rounded-3xl font-bold" >
           Back
         </button>
-      </div>
+      </div> */}
     </div>
   );
 };
